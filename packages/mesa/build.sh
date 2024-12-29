4b2675608f9065d6be86b38ff7b11d172d0010b8
Adya Roby <adyaro37@gmail.com>
2024-12-30 02:01:28 +0700

    Update build.sh

--- packages/mesa/build.sh
+++ packages/mesa/build.sh
@@ -39,7 +39,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_post_get_source() {
 	# Do not use meson wrap projects
 	rm -rf subprojects
-        cp cso_context.c /home/builder/.termux-build/mesa/src/src/gallium/auxiliary/cso_cache/cso_context.c
+        cp $TERMUX_PKG_BUILDER_DIR/cso_context.c /home/builder/.termux-build/mesa/src/src/gallium/auxiliary/cso_cache/cso_context.c
 }
 
 termux_step_pre_configure() {
